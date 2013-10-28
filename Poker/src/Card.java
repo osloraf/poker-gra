77fd18389c618fd90745e7337f7d9ceb6a1c1898
gravoo <gravoo@yahoo.com>
2013-10-28 12:02:45 +0000

    Card

---
+++ Poker/src/Card.java
@@ -0,0 +1,21 @@
+import java.awt.image.BufferedImage;
+
+/**
+ * 
+ */
+
+/**
+ * @author gravoo
+ *
+ */
+public class Card {
+	int value;
+	short color;
+	BufferedImage img = null;
+	Card(int val,short col){
+		value=val;
+		col=col;
+	}
+	
+
+}
